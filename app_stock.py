9e7b24bc669df6fabfb62e4624c8746f86aadbdd
dennx2 <jdcht1228@gmail.com>
2024-06-14 21:34:45 -0400

    Implemented function that calculates stochastic value

--- app_stock.py
+++ app_stock.py
@@ -2,10 +2,10 @@ from tracker.stock.get_data import *
 from tracker.stock.technical_analysis import *
 
 df = get_data("00934.TW", period="1mo")
-# prerequisite: get historical quotes from your own source
 quotes = transform_data_to_quotes(df)
-# example: get 20-period simple moving average
-result = run_sma(quotes, 5, candle_part=CandlePart.CLOSE)
-# use results as needed for your use case (example only)
 
-print(f"SMA on {result.date.date()} was ${result.sma or 0:.4f}")
+sma_result = run_sma(quotes, 5)
+print(f"SMA on {sma_result.date.date()} was ${sma_result.sma or 0:.4f}")
+
+stoch_result = run_stoch(quotes, speed="fast")
+print(f"{stoch_result.date.date()} | K:{stoch_result.k:.2f} | D:{stoch_result.d:.2f}")
--- tracker/stock/technical_analysis.py
+++ tracker/stock/technical_analysis.py
@@ -1,19 +1,39 @@
-from datetime import date
+from typing import Literal
 from stock_indicators import indicators
 from stock_indicators.indicators.common.quote import Quote
 from stock_indicators.indicators.common.enums import CandlePart
+from stock_indicators.indicators.common.enums import MAType
 from stock_indicators.indicators.sma import SMAResult
+from stock_indicators.indicators.stoch import StochResult
 
 from ..utils.logger import setup_logger
 
 logger = setup_logger()
 
 
-def run_stoch(quotes: Quote):
-    pass
+def run_stoch(
+    quotes: list[Quote], speed: Literal["slow", "fast"] = "slow"
+) -> StochResult:
+
+    if speed == "slow":
+        smooth_periods = 3
+    else:
+        smooth_periods = 1
+
+    results = indicators.get_stoch(
+        quotes,
+        lookback_periods=14,
+        signal_periods=3,
+        smooth_periods=smooth_periods,
+        ma_type=MAType.SMA,
+    )
+
+    return results[-1]
 
 
-def run_sma(quotes: list[Quote], periods: int, candle_part: CandlePart) -> SMAResult:
+def run_sma(
+    quotes: list[Quote], periods: int, candle_part: CandlePart = CandlePart.CLOSE
+) -> SMAResult:
 
     if periods >= len(quotes):
         logger.error(
@@ -21,9 +41,13 @@ def run_sma(quotes: list[Quote], periods: int, candle_part: CandlePart) -> SMARe
         )
         raise ValueError(f"Something wrong with sma calculation.")
 
-    results = indicators.get_sma(quotes, periods, candle_part=candle_part)
+    results = indicators.get_sma(
+        quotes, lookback_periods=periods, candle_part=candle_part
+    )
     if not results:
-        logger.error(f"No sma result")
-        raise ValueError(f"Something wrong with sma calculation.")
+        logger.error(
+            f"No SMA results returned for periods={periods}, candle_part={candle_part}."
+        )
+        raise ValueError("Failed to calculate SMA.")
 
     return results[-1]
